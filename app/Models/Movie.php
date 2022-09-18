b9cbf42af1c4d4463bd6204e8d13e039376a5ca1
tmking627 <tmy6254789@gmail.com>
2022-09-18 21:33:39 +0900

    Movieモデルの作成

---
+++ app/Models/Movie.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'description',
+        'url'
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
