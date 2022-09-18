302c42aa352fdb5a532e859308cac5ef7d3b8f64
tmking627 <tmy6254789@gmail.com>
2022-09-18 21:33:14 +0900

    マイグレーションの作成

---
+++ database/migrations/2022_09_13_231829_create_movies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned()->nullable(false);
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('title',256)->nullable(false);
+            $table->text('description')->nullable();
+            $table->text('url')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
