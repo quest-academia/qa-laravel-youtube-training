48d1d844d115554243a4e9f2e60078f816852707
Tacomazine <qqpw8gd9k@triton.ocn.ne.jp>
2022-10-05 14:18:56 +0000

    create:ヘッダー・フッター・ログイン画面（フロント部分）の実装

---
+++ resources/views/commons/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="fixed-bottom">
+    <nav class="navbar navbar-expand navbar-light bg-warning">
+        <a class="navbar-brand">私立探求学園</a>
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item font-small">
+                ©2022 QuestAcademia, All rights reserved.
+            </li>
+        </ul>
+    </nav>
+</footer> 
---
+++ resources/views/commons/header.blade.php
@@ -0,0 +1,18 @@
+<header class="mb-5">
+    <nav class="navbar navbar-expand navbar-light bg-warning">
+        <a class="navbar-brand" href="/">私立探求学園</a>
+        <!-- <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-md-auto align-items-center">
+                <li>
+                    <a class="btn btn-info" href=""><i class="fas fa-pencil-alt"></i>
+                    投稿する
+                    </a>
+                </li>
+                <li>
+                    <a class="nav-link far fa-user fa-lg" href="">
+                    </a>
+                </li>
+            </ul>
+        </div> -->
+    </nav>
+</header>
