f08e4cb87ec238329fc5beac325a6cbc7621cd4a
tmking627 <tmy6254789@gmail.com>
2022-10-23 11:06:57 +0900

    検索機能の作成

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -7,9 +7,31 @@ use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $movies = Movie::with('user:id,name')->latest()->paginate(12);
-        return view('movie.index',compact('movies'));
+        $query = Movie::query()->with('user:id,name');
+        $search = $request->input('search');
+
+        //検索機能
+        if(!empty($search)){
+            // 単語を半角スペースで区切り、配列にする
+            $space_search = mb_convert_kana($search, 's');
+            $search_words_array = preg_split('/[\s,]+/', $space_search, -1, PREG_SPLIT_NO_EMPTY);
+
+            // 単語をループで回し、それぞれの単語で部分一致で検索
+            foreach ($search_words_array as $value) {
+                $query->where('title', 'like', '%' . $value . '%')
+                ->orWhere('description', 'like', '%' . $value . '%')
+                ->orWhereHas(
+                    'user',
+                    function ($query) use ($value) {
+                        $query->where('name', 'like', '%' . $value . '%');
+                    }
+                );
+            }
+        }
+
+        $movies = $query->latest()->paginate(12);
+        return view('movie.index', compact('movies', 'search'));
     }
 }
