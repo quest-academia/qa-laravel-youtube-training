c64e93d2e148e59057a855b6f59c60e2021b0f7c
tomoya_okura <tomoya.okura@digitalidentity.co.jp>
2022-10-30 04:04:19 +0900

    ヘッダーに動画一覧のリンクを追加

--- resources/views/commons/header.blade.php
+++ resources/views/commons/header.blade.php
@@ -1,9 +1,10 @@
 <header class="mb-5">
     <nav class="navbar navbar-expand navbar-light bg-warning">
         <a class="navbar-brand" href="/">私立探求学園</a>
-        <!-- <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ml-md-auto align-items-center">
-                <li>
+                <li><a class="nav-item nav-link" href="{{ route('movie.index') }}">動画一覧</a></li>
+                <!-- <li>
                     <a class="btn btn-info" href=""><i class="fas fa-pencil-alt"></i>
                     投稿する
                     </a>
@@ -11,8 +12,8 @@
                 <li>
                     <a class="nav-link far fa-user fa-lg" href="">
                     </a>
-                </li>
+                </li> -->
             </ul>
-        </div> -->
+        </div>
     </nav>
 </header>
