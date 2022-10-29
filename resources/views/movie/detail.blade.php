14b429a0de9692b446037003d5e69fc6f4aad407
tomoya_okura <tomoya.okura@digitalidentity.co.jp>
2022-10-29 12:30:02 +0900

    空行インデント修正

--- resources/views/movie/detail.blade.php
+++ resources/views/movie/detail.blade.php
@@ -2,9 +2,9 @@
 @section('content')
 <a class="" href="{{route('movie.index')}}">&lt&lt一覧に戻る</a>
 <h1 class="mt-3">{{$movie->title}}</h1>
- <div class="d-flex justify-content-center">
- <iframe src="{{$movie->getYoutubeEmbedURL()}}" width="560" height="315" frameborder="0" class="mx-auto w-60 mw-100 m-5"></iframe>
- </div>
+<div class="d-flex justify-content-center">
+    <iframe src="{{$movie->getYoutubeEmbedURL()}}" width="560" height="315" frameborder="0" class="mx-auto w-60 mw-100 m-5"></iframe>
+</div>
 <h2>概要</h2>
 <p>{!! nl2br(htmlspecialchars($movie->description)) !!}</p>
 {{-- @if($movie->user_id === Auth::user()->id) --}}
